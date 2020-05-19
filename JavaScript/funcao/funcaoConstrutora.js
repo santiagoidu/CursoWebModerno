ff8d5f7a0657d52962df1215f6513b81272a2bf3
santiagoidu <sr_santiago@icloud.com>
2020-05-19 17:59:42 -0300

    27° commit

---
+++ JavaScript/funcao/funcaoConstrutora.js
@@ -0,0 +1,34 @@
+function Carro(velocidadeMaxima = 200, delta = 5) {
+    //atributo privado
+    let velocidadeAtual = 0
+    // metoddo publico
+    this.acelerar = function () {
+        if (velocidadeAtual + delta <= velocidadeMaxima){
+            velocidadeAtual += delta 
+        } else {
+            velocidadeAtual = velocidadeMaxima 
+        }
+    }
+    // metodo publico 
+    this.getVelocidadeAtual = function () {
+        return velocidadeAtual + ' Km/h'
+    }
+}
+
+const uno = new Carro
+uno.acelerar()
+uno.acelerar()
+uno.acelerar()
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new Carro(350, 20)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
+
+
+console.log(typeof Carro)
+console.log(typeof ferrari)
