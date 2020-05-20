d1f13c2e750e65de9c1f9be9f4cbcce7d6218b78
santiagoidu <sr_santiago@icloud.com>
2020-05-20 16:48:13 -0300

    30° commit

--- JavaScript/exercicios/ex002.js
+++ JavaScript/exercicios/ex002.js
@@ -8,3 +8,54 @@
 ao tamanho de seus lados. (Neste exemplo deve-se abstrair as condições matemáticas de existência de um
 triângulo). */
 
+
+function triangulo(x , y , z){
+    if (x === y && x === z && y === z){
+        return `
+        _______________________________________________________________
+        |                     Triângulo Equilátero.                   |
+        |-------------------------------------------------------------|
+        |Os três lados são iguais:                                    |
+        |O lado X possui ${x} de tamanho.                                |
+        |O lado Y possui ${y} de tamanho.                                |
+        |O lado Z possui ${z} de tamanho.                                |
+        |-------------------------------------------------------------|
+        `
+    } else if (x === y || x === z || z === y){
+       return `
+        _____________________________________________________________
+        |                      Triângulo Isósceles.                  |
+        |------------------------------------------------------------|
+        |Dois lados são iguais:                                      |
+        | O lado X possui ${x} de tamanho.                              |
+        | O lado Y possui ${y} de tamanho.                              |
+        | O lado Z possui ${z} de tamanho.                              |
+        |------------------------------------------------------------|
+       `
+    } else if ( x != y && x != z && z != y) {
+        return `
+        ______________________________________________________________
+        |                      Triângulo Escaleno.                    |
+        |-------------------------------------------------------------|
+        |Todos os lados são diferentes:                               |
+        |O lado X possui ${x} de tamanho.                                |
+        |O lado Y possui ${y} de tamanho.                                |
+        |O lado Z possui ${z} de tamanho.                               |
+        |-------------------------------------------------------------|
+        `
+    } else if ( x !== Number || y !== Number || z !== Number){
+        return `
+        _______________________________________
+       |                Atenção!!!             |
+       |---------------------------------------|
+       | Verifique os dados e tente novamente! |
+       |---------------------------------------|
+       `
+    }
+}
+
+console.log(triangulo(5, 5, 5))
+console.log(triangulo(5, 4, 5))
+console.log(triangulo(1, 5, 10))
+console.log(triangulo([], '', '0'))
+
---
+++ JavaScript/exercicios/ex003.js
@@ -0,0 +1,7 @@
+//Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente
+
+function calc(base, expoente){
+    return base ** expoente
+}
+
+console.log(calc(2,5))
