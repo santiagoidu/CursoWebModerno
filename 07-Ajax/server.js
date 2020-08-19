9c0b82b76d590bec2abbe611fb80a8ea321d7334
santiagoidu <sr_santiago@icloud.com>
2020-08-19 18:43:00 -0300

    Add Ajax - Axios 02

--- 07-Ajax/server.js
+++ 07-Ajax/server.js
@@ -37,4 +37,14 @@ app.post('/formulario', (req, res) => {
         id: 1
     })
 })
+
+app.get('/parOuImpar', (req, res) => {
+    //req.body
+    //req.query
+    //req.parans
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
 app.listen(8080, () => console.log('Executando...'))
