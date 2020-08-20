ac11703effe122e704d5f1208b3b73c177485e18
santiagoidu <sr_santiago@icloud.com>
2020-08-20 19:59:26 -0300

    Add Gulp - Processando SASS com Gulp

--- 08-Gulp/gulp/css/gulpfile.js
+++ 08-Gulp/gulp/css/gulpfile.js
@@ -1,4 +1,4 @@
-const { series } = require('gulp')
+const { parallel } = require('gulp')
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const uglifycss = require('gulp-uglifycss')
@@ -12,4 +12,9 @@ function transformacaoCSS() {
         .pipe(gulp.dest('build/css'))
 }
 
-exports.default = series(transformacaoCSS)
+function copiarHTML() {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('build'))
+}
+
+exports.default = parallel(transformacaoCSS, copiarHTML)
