ad27fe409c4a234cc7831c9dbf2ce39cdd00390c
santiagoidu <sr_santiago@icloud.com>
2020-08-20 17:13:20 -0300

    Add Gulp - Fundamentos de Gulp

--- 08-Gulp/gulp/basico/gulpfile.js
+++ 08-Gulp/gulp/basico/gulpfile.js
@@ -1,9 +1,29 @@
 const gulp = require('gulp')
-const  series = gulp.series
+const { series, parallel } = require('gulp')
 
-function copiar(cb){
-    console.log('Tarefa de copiar!')
+const antes1 = cb => {
+    console.log('Tarefa Antes 1!')
     return cb()
 }
 
-module.exports.default = series(copiar)
+const antes2 = cb => {
+    console.log('Tarefa Antes 2!')
+    return cb()
+}
+
+function copiar(cb){
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    gulp.src('pastaA/**/*.txt')
+    .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tarefa FIM!')
+    return cb()
+}
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim,)
