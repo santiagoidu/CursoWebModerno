c9d615341b291a29d3c2f9246da259c537c9470e
santiagoidu <sr_santiago@icloud.com>
2021-08-13 16:53:23 -0300

    Add Pai & Filho #1

--- 12-React/exercicios/src/index.js
+++ 12-React/exercicios/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Saudacao from './components/Saudacao'
+import Pai from './components/Pai'
 
 
 
 ReactDOM.render(
     <div>
-        <Saudacao tipo="Bom dia" nome="João" />
+        <Pai nome="Paulo" sobrenome="Silva" />
     </div>,
     document.getElementById('root')
 )
