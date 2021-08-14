5345da224255da09aae0dc2e41fb8cb2457e3cb6
santiagoidu <sr_santiago@icloud.com>
2021-08-14 18:12:01 -0300

    Add Logic #1

--- 12-React/calculadora/src/components/Button.jsx
+++ 12-React/calculadora/src/components/Button.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 import './Button.css'
 
-export default props => 
-<button className="button">{props.label}</button>
+export default props => {
+    return (        
+<button className={`
+    button 
+    ${props.operation? 'operation' : ''}
+    ${props.double ? 'double' : ''}
+    ${props.triple ? 'triple' : ''}`}
+onClick={e => props.click && props.click(props.label)}
+>
+    {props.label}
+</button>
+    )
+}
--- 12-React/calculadora/src/index.js
+++ 12-React/calculadora/src/index.js
@@ -6,7 +6,7 @@ import Calculator from './main/Calculator';
 ReactDOM.render(
   <div>
     <h1>Calculadora</h1>
-    <Calculator />,
+    <Calculator />
   </div>,
   document.getElementById('root')
 );
