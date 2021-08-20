f9e3e04837adef29a7e79557e41d95f9b2ab3a71
santiagoidu <sr_santiago@icloud.com>
2021-08-20 17:28:47 -0300

    Add Component Cabeçalho

--- 12-React/crud/frontend/src/main/App.jsx
+++ 12-React/crud/frontend/src/main/App.jsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
 
@@ -11,6 +13,6 @@ export default props =>
 <div className="app">
     <Logo />
     <Nav />
-    <Main />
+    <Main icon="home" title="Início" subtitle="Segundo Projeto do capítulo do React "/>
     <Footer />
 </div>
