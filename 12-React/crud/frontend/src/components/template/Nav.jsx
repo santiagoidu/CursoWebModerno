06efc9477b9dad45a04e8fc2d7e6080c98fe187a
santiagoidu <sr_santiago@icloud.com>
2021-08-25 10:34:09 -0300

    Add Component Nav

--- 12-React/crud/frontend/src/components/template/Nav.jsx
+++ 12-React/crud/frontend/src/components/template/Nav.jsx
@@ -3,5 +3,10 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        <a href="#/">
+            <i className="fa fa-home"></i> Início
+        </a>
+        <a href="#/users">
+        <i className="fa fa-users"></i> Usuários
+        </a>
     </aside>
